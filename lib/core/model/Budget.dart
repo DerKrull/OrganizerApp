a9742a50dccb67b8da18277ae965280b5527a394
FelixKrull <ls2016@web.de>
2023-01-14 19:09:59 +0100

    Add Budget data Model

---
+++ lib/core/model/Budget.dart
@@ -0,0 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Budget {
+  DateTime  date;
+  double value;
+
+  Budget(this.date, this.value);
+
+  factory Budget.fromDocumentSnapshot({required DocumentSnapshot<Map<String, dynamic>> doc}) {
+    Timestamp timestamp = doc.data()!['date'];
+    DateTime date = DateTime.fromMillisecondsSinceEpoch(timestamp.millisecondsSinceEpoch);
+    var tmpValue = doc.data()!['value'];
+    if(tmpValue.runtimeType == int) {
+      tmpValue = tmpValue.toDouble();
+    } else if (tmpValue.runtimeType != double) {
+      throw TypeError();
+    }
+    return Budget(date, tmpValue);
+  }
+}
+
