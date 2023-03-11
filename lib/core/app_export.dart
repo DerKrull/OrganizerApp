a071f44a98ef4d79e2c19904c70b58bf2be2eeb1
FelixKrull <ls2016@web.de>
2023-03-11 01:26:24 +0100

    Delete old, unused constants and widgets

--- lib/core/app_export.dart
+++ lib/core/app_export.dart
@@ -1,7 +1,5 @@
 export 'package:organizer_app/core/constants/constants.dart';
 
-export '/core/utils/image_constant.dart';
-export '/core/utils/color_constant.dart';
 export '/core/utils/materialThemeColorConstant.dart';
 export '/core/utils/size_utils.dart';
 export '/core/utils/TimeUtils.dart';
@@ -12,5 +10,3 @@ export '/core/FireStoreFutures/AddObjects.dart';
 export '/core/FireStoreFutures/UpdateObjects.dart';
 export '/core/FireStoreFutures/GetObjects.dart';
 export '/core/FireStoreFutures/DeleteObjects.dart';
-
-export '/widgets/custom_image_view.dart';
